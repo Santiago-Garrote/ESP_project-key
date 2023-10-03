81eec95bd885c22185891cb5b9c88e3d59a86e19
Santiago-Garrote <130693515+Santiago-Garrote@users.noreply.github.com>
2023-10-03 16:38:08 -0300

    Update Master_ESP8266_modificado.ino

--- Master_ESP8266_modificado.ino
+++ Master_ESP8266_modificado.ino
@@ -1,68 +1,79 @@
-/*
-* Para LOLIN D1 mini clone, FUNCIONA TAMBIEN CON esp01
-* Envia un dato cada 20 segundos
-*/
- 
 #include <ESP8266WiFi.h>
 #include <espnow.h>
- 
+#include <iostream>
+
+using namespace std;
+
+string encryptDecrypt(string toEncrypt) {
+    char key = 'K'; //Any char will work
+    string output = toEncrypt;
+    
+    for (int i = 0; i < toEncrypt.size(); i++)
+        output[i] = toEncrypt[i] ^ key;
+
+    
+    return output;
+}
+
+
 // Reemplazar con la MAC Address del ESP que recibe
-uint8_t broadcastAddress[] = {0xe0, 0x5a, 0x1b, 0xa1, 0x0c, 0xd8};
+uint8_t broadcastAddress[] = { 0xe0, 0x5a, 0x1b, 0xa1, 0x0c, 0xd8 };
 
-// LMK keys
-static const char* LMK_KEY_STR = "DONT_BE_LAZY_OK?";
+// LMK and PMK keys
+static const char *PMK_KEY_STR = "PrivateKey";
+static const char *LMK_KEY_STR = "PublicKey";
 
-int cuenta=0;
+int cuenta = 0;
 // Creo una estructura para enviar los datos
 // esta debe estar replicada en el receptor
 typedef struct struct_message {
-char palabra[6];
+  char palabra[6];
   int numero;
 } struct_message;
- 
+
 // Creo la estructura para enviar los datos
 struct_message myData;
- 
+
 // Funcion que se ejecuta para enviar el dato
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print(cuenta);
   Serial.print(" Mensaje enviado: ");
-  if (sendStatus == 0){
+  if (sendStatus == 0) {
     Serial.println("Ok ");
-  }
-  else{
+  } else {
     Serial.println("Fallo ");
   }
   cuenta++;
 }
-  
+
 void setup() {
-  
+
   Serial.begin(115200);
   
   // Defino el funcionamiento del ESP01 como Wi-Fi Station
   WiFi.mode(WIFI_STA);
- 
+
   // Init ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error inicializando ESP-NOW");
     return;
   }
- 
-  
+
+
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_send_cb(OnDataSent);
-   
+
   // Registro el receptor
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-  // Valores a enviar
-  strcpy(myData.palabra, "39552");
-  myData.numero =5;
-   
-  // envio el mensaje via ESP-NOW
-  esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-}
-  
-void loop() {
 
+  // Valores a enviar
+  string encrypted = encryptDecrypt("HolaYisus");
+  strcpy(myData.palabra, encrypted.c_str());
+  myData.numero = 5;
+
+  // envio el mensaje via ESP-NOW
+  esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+}
+
+void loop() {
 }
